7de84769b0eaa4b9f57ccf4a8738aafcd6cb40eb
Caudrey <103728671+Caudrey@users.noreply.github.com>
2023-05-04 12:22:32 +0700

    master dkk

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,16 @@
+{{-- Footer --}}
+<div class="container pt-3">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-4 d-flex align-items-center">
+        <span class="mb-3 mb-md-0 text-white">&copy; 2023 Cecil's Profile</span>
+      </div>
+
+      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3"><a class="text-white" href="/contact"><svg class="bi" width="28" height="28"><use xlink:href="#email"/></svg></a></li>
+        <li class="ms-3"><a class="text-white" href="/contact"><svg class="bi" width="28" height="28"><use xlink:href="#WA"/></svg></a></li>
+        <li class="ms-3"><a class="text-white" href="/contact"><svg class="bi" width="28" height="28"><use xlink:href="#line"/></svg></a></li>
+        <li class="ms-3"><a class="text-white" href="/contact"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+      </ul>
+    </footer>
+</div>
+{{-- End of Footer --}}
---
+++ resources/views/header.blade.php
@@ -0,0 +1,11 @@
+{{-- Header --}}
+<div class="container mt-1">
+    <header class="py-3 mb-4 border-bottom">
+        <div class="container d-flex flex-wrap justify-content-center">
+            <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
+                <span class="fs-4 text-white">@yield('title')</span>
+            </a>
+        </div>
+    </header>
+</div>
+{{-- End Of Header --}}
